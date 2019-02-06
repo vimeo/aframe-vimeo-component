a1c3b37716551f1ef91d6bef1c1a9dd54621e976
juniorxsound <fleisher.or@gmail.com>
2019-02-06 11:34:38 -0500

    Basic project structure to build, submodule and link examples to dist for aframe-vimeo-component 🎉

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const Vimeo = require('../ext/vimeo-threejs-player/dist/vimeo_threejs_player');
 
 AFRAME.registerComponent('vimeo', {
   init: function () {
-    console.log('Hello, Vimeo!');
+    console.log('Hello, Vimeo!', Vimeo);
   }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
 const path = require('path');
 const webpack = require('webpack');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+
+let libraryName = pkg.name;
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+}
 
 module.exports = {
   context: path.join(__dirname, './'),
   entry: './src/index.js',
+  mode: mode,
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'aframe-vimeo-component.js',
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this"
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         loader: 'jsx-loader',
