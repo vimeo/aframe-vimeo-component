d6829e867bd7fe1f7a42a551eebeffe9dd097b3c
juniorxsound <fleisher.or@gmail.com>
2019-02-22 15:55:16 -0500

    Adding a console warning with a Getting Started guide link when the host is not our Glitch domain

---
+++ assets/js/host-console-warning.js
@@ -0,0 +1,5 @@
+var host = window.location.hostname;
+
+if (host != 'aframe-vimeo-component.glitch.me') {
+  console.warn("[Vimeo] Remixed the example?  don't forget to generate your own token and change the Vimeo video id in the example files. \n Check out our Getting Started guide for more info: \n https://github.com/vimeo/aframe-vimeo-component/wiki/Getting-Started-%F0%9F%9A%80.md")
+}
