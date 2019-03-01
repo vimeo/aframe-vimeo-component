51001720ccae4aabaf586028123551611a84730f
juniorxsound <fleisher.or@gmail.com>
2019-03-01 11:45:01 -0500

    Adding an npm start script to change the README contents on Glitch 📝

---
+++ assets/js/readme-glitch-fix.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+require('dotenv').config();
+
+var glitchReadMeText = "### Hello and welcome to the Vimeo A-Frame component examples 👋🏼 \n Have a look at our [Getting Started guide](https://github.com/vimeo/aframe-vimeo-component/wiki/Getting-Started-%F0%9F%9A%80), or check out the [full documentation in Github](https://github.com/vimeo/aframe-vimeo-component). \n For questions and support, open a Github issue [StackOverflow](https://github.com/vimeo/aframe-vimeo-component/issues/new)";
+
+if (process.env.ENV === 'Glitch') {
+  console.log('[Ops] Changing the README in Glitch to point to Github documentation');
+
+  // Change the README to the Glitch one
+  fs.writeFile('./README.md', glitchReadMeText, 'utf8', function (err) {
+     if (err) return console.log(err); 
+  });  
+}
